88b06f62b326baadb6141f496a905bacdbfad901
JonasCarlstroem <82809067+JonasCarlstroem@users.noreply.github.com>
2025-03-28 12:49:11 +0100

    changes

--- lib/gen/generator.cpp
+++ lib/gen/generator.cpp
@@ -2,6 +2,7 @@
 #include <tinyxml2.h>
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 
 using namespace tinyxml2;
 
@@ -12,11 +13,13 @@ void generate_cpp(const std::string& fileName, tinyxml2::XMLDocument& doc)
 
 int main()
 {
-    const char* dir[MAX_PATH];
-    if (GetCurrentDirectory(MAX_PATH, &dir))
+    char buf = { 0 };
+    if (!GetCurrentDirectory(MAX_PATH, &buf))
     {
-
+        return -1;
     }
+    std::string str(&buf);
+
     tinyxml2::XMLDocument doc;
     if (doc.LoadFile("test.xml") != XML_SUCCESS)
     {
